7a179dc33d63832a5d173a330cddfee138e1487c
DavidHCCNguyen <creativespecials@gmail.com>
2023-07-24 18:22:50 +0930

    Adding server.js

--- 1 Assistance.md
+++ 1 Assistance.md
@@ -1,3 +1,4 @@
+# Url Paths
 ```
 /api/users (GET) - Get all users
 
@@ -26,4 +27,22 @@
 /api/thoughts/:thoughtId/reactions (POST) - Create a reaction stored in a single thought's reactions array field
 
 /api/thoughts/:thoughtId/reactions/:reactionId (DELETE) - Pull and remove a reaction by the reaction's reactionId value
+```
+
+# Meanings of Folders and Js
+
+```
+models/User.js - Define the User model using Mongoose schema.
+
+models/Thought.js - Define the Thought model using Mongoose schema and the Reaction subdocument schema.
+
+routes/userRoutes.js - Implement the API routes for handling users, including GET, POST, PUT, and DELETE operations.
+
+routes/thoughtRoutes.js - Implement the API routes for handling thoughts and reactions, including GET, POST, PUT, and DELETE operations.
+
+controllers/userController.js - Implement the controller functions for user-related operations.
+
+controllers/thoughtController.js - Implement the controller functions for thought-related operations.
+
+utils/date.js - Create utility functions for handling timestamps and date formatting (optional).
 ```
--- server.js
+++ server.js
@@ -1,46 +1,32 @@
-const path = require('path');
 const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
-
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
+const thoughtRoutes = require('./routes/thoughtRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
-// Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
-
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {
-    maxAge: 300000,
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
-  },
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
-
-app.use(session(sess));
-
-// Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+// Connect to MongoDB
+mongoose
+  .connect('mongodb://localhost:27017/my_social_network', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    // Start the server after successfully connecting to the database
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err.message);
+  });
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+// Define routes
+app.use('/api/users', userRoutes);
+app.use('/api/thoughts', thoughtRoutes);
